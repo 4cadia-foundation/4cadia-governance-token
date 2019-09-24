6b048ded5353d44d2a9d600d8c6dbea8a4d7b6fd
Paulo Gabriel <paulo@janusproj.com>
2019-09-24 19:24:20 -0300

    implemented ERC223 standart

---
+++ contracts/token/FGToken.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+import "./ERC223Detailed.sol";
+import "./ERC223.sol";
+import "./ERC223Mintable.sol";
+
+contract FGToken is ERC223Mintable ,ERC223Detailed {
+
+    constructor (string memory _name, string memory _symbol, uint8 _decimals, uint256 initialSupply) 
+    ERC223Detailed(_name,_symbol,_decimals) public {
+        mint(msg.sender, initialSupply);
+    }
+
+    function mint(address account, uint256 amount) public onlyMinter returns (bool) {
+        
+        require(account != address(0), "ERC20: mint to the zero address");
+        _totalSupply = _totalSupply.add(amount);
+        balances[account] = balances[account].add(amount);
+        
+        return true;
+    }
+
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 const FGToken = artifacts.require("FGToken");
 
 module.exports = function(deployer, network, accounts) {
+  
   const _name = 'FGToken';
   const _symbol = 'FGT';
   const _decimals = 8; 
-  deployer.deploy(FGToken, _name, _symbol, _decimals);
+  const _initialSupply = 0;
+  
+  deployer.deploy(FGToken, _name, _symbol, _decimals, _initialSupply);
 };
